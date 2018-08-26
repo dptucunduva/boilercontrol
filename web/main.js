0aa3acd0ec80d2a92b7c338221f62f6a780a162f
root <root@ip-172-31-0-243.sa-east-1.compute.internal>
2018-08-26 23:28:53 +0000

    Securing browser websocket

--- web/main.js
+++ web/main.js
@@ -6,7 +6,9 @@ var basicAuth = require('express-basic-auth')
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
 var appRedirect = express();
 
 // HTTP Server. Just redirect to HTTPS
@@ -26,10 +28,23 @@ const credentials = {
 const httpsServer = https.createServer(credentials, app)
 httpsServer.listen(443);
 
+// Authentication
+const authProp = JSON.parse(fs.readFileSync('auth.json','utf8'));
+app.use('/',basicAuth(authProp));
+
 // Browser socket. Send information to the browser
 const ioBrowserServer = https.createServer(credentials)
 ioBrowserServer.listen(8098);
 var browser = require('socket.io')(ioBrowserServer);
+browser.on('connect', function(socket) {
+	var ioCookieHeader = socket.handshake.headers.cookie;
+	var auth = authProp.authorization;
+	if (typeof ioCookieHeader == 'undefined' ||
+	    typeof auth == 'undefined' ||
+	    decodeURIComponent(ioCookieHeader).indexOf(auth) <= 0) {
+		socket.destroy();
+	}
+})
 
 // Admin socket. Wait for connections from internal agent with current data
 var actuatorData;
@@ -43,52 +58,49 @@ admin.on('connect', function(socket) {
 	})
 })
 
-// Authentication
-var authProp = JSON.parse(fs.readFileSync('auth.json','utf8'));
-app.use('/',basicAuth(authProp));
-
 // Public static files
 app.use(express.static('public'));
 
 // Write current data
-function writeData(res) {
+function writeData(req, res) {
 	res.setHeader('Content-Type', 'application/json');
 	res.setHeader('Cache-Control', 'no-cache');
 	res.setHeader('Connection', 'closed');
+	res.cookie('auth', req.headers.authorization, {httpOnly: true});
 	res.send(actuatorData);
 }
 
 // Get current status
 app.get('/status', function (req, res) {
-	writeData(res);
+	writeData(req, res);
 })
 
 // Heater on
 app.put('/heater/on/:time', function (req, res) {
 	admin.emit('heaterOn', req.params.time);
-	writeData(res);
+	writeData(req, res);
 })
 
 // Heater off
 app.put('/heater/off/:time', function (req, res) {
 	admin.emit('heaterOff', req.params.time);
-	writeData(res);
+	writeData(req, res);
 })
 
 // Heater auto
 app.put('/heater/auto', function (req, res) {
 	admin.emit('heaterAuto');
-	writeData(res);
+	writeData(req, res);
 })
 
 // Pump off
 app.put('/pump/off/:time', function (req, res) {
 	admin.emit('pumpOff', req.params.time);
-	writeData(res);
+	writeData(req, res);
 })
 
 // Pump auto
 app.put('/pump/auto', function (req, res) {
 	admin.emit('pumpAuto');
-	writeData(res);
+	writeData(req, res);
 })
