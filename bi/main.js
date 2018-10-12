e0cfde603bb17230f3262f73a976fbb9a9b0399d
Daniel Parra <dptucunduva@gmail.com>
2018-10-12 12:22:03 -0300

    Adding module that reads data and inserts on a mongodb

---
+++ bi/main.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env nodejs
+
+const fs = require('fs');
+const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient;
+
+// Data files location
+const powerDatadir = '../internal/data/power';
+const temperatureDatadir = '../internal/data/temperature';
+
+// Mongo
+const mdbUrl = 'mongodb://italopulga.ddns.net:57017';
+const mdbClient = new MongoClient(mdbUrl);
+
+// Handle files
+function loadData() {
+
+	mdbClient.connect(function(err) {
+		assert.equal(null, err);
+
+		// For each file in power folder
+		fs.readdirSync(powerDatadir).forEach(file => {
+	  		var powerData = JSON.parse(fs.readFileSync(powerDatadir+'/'+file,'utf8'));
+	  		addToDB('powerdb', 'readings', powerData, powerDatadir+'/'+file);
+		});
+
+		// For each file in temperature folder
+		fs.readdirSync(temperatureDatadir).forEach(file => {
+	  		var temperatureData = JSON.parse(fs.readFileSync(temperatureDatadir+'/'+file,'utf8'));
+	  		addToDB('temperaturedb', 'readings', temperatureData, temperatureDatadir+'/'+file);
+		});
+
+		mdbClient.close();
+	});
+}
+loadData();
+setInterval(loadData, 60000);
+
+// Add entry to DB
+function addToDB(dbName, collectionName, data, fileToDel) {
+	const collection = mdbClient.db(dbName).collection(collectionName);
+	collection.insertOne(data, function(err, result) {
+		assert.equal(err, null);
+		assert.equal(1, result.result.n);
+		assert.equal(1, result.ops.length);
+	  	fs.unlinkSync(fileToDel);
+	});
+}
